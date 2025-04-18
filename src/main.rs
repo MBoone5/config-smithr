490c8387d0d92df2a4f2c72a70697d3c259ca753
Sal Boone <msb@msboone.dev>
2025-04-18 04:36:18 -0700

    Add git2, accept path to repo and check repo is bare

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use clap::Parser;
+use git2::Repository;
 use std::path::PathBuf;
 
 // Struct to encapsulate cfg/dot file info
 #[derive(Parser)]
 struct Forge {
-    pattern: String,
     path: PathBuf,
+}
+
+// TODO: Figure out how to unwrap `open_bare` into repo.namespace() to get repo name
+fn check_repo(path: PathBuf) {
+    let repo_result = Repository::open_bare(path);
+    let repo = match repo_result {
+        Ok(repo) => repo,
+        Err(error) => {
+            panic!("nah nah nah nah: {}", error);
+        }
+    };
+
+    println!("{}", repo.is_bare());
 }
 
 fn main() {
     let args = Forge::parse();
-    println!("pattern: {:?}", args.pattern);
+
+    println!("Reading repo at {:?} ... ", args.path);
+    check_repo(args.path);
 }
