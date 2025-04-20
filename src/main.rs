16d769277528ea469b57b5765dead1fe62f1c00b
Sal Boone <msb@msboone.dev>
2025-04-19 23:35:26 -0700

    refactor(main): factor out 'check_repo' to a single validate_repo(&repo) call for the use of adding more checks later

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,82 @@
 use clap::Parser;
 use git2::Repository;
-use std::path::PathBuf;
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+    result::Result,
+};
 
 // Struct to encapsulate cfg/dot file info
 #[derive(Parser)]
-struct Forge {
+struct CliArgs {
     path: PathBuf,
 }
 
-// TODO: Figure out how to unwrap `open_bare` into repo.namespace() to get repo name
-fn check_repo(path: PathBuf) {
-    let repo = match Repository::open(path) {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
+struct ConfigFile {
+    file: File,
+}
 
-    let repo_name = repo
-        .path()
+struct ConfigSet {
+    file_set: Vec<ConfigFile>,
+}
+
+struct Settings {
+    repo: Repository,
+}
+
+struct ForgeState {
+    file_set: ConfigSet,
+    user_settings: Settings,
+}
+
+fn _get_repo_name(path: &Path) -> Result<String, String> {
+    let repo_name = path
         .file_name()
         .and_then(|repo_os_str| repo_os_str.to_str());
 
-    println!("Bare repo? = {}", repo.is_bare());
+    let result = match repo_name {
+        None => {
+            return Err(
+                "Repo does not have a path on filesytem -- check your args passed when calling"
+                    .to_string(),
+            );
+        }
+        Some(name) => name.to_string(),
+    };
 
-    match repo_name {
-        None => println!("Repo name not found, check filesystem"),
-        Some(name) => println!("Repo name? = {}", name),
-    }
+    Ok(result)
+}
+
+fn validate_repo(repo: &Repository) -> bool {
+    return repo.path().is_dir() && repo.is_bare();
+}
+
+fn get_repo(path: PathBuf) -> Result<Repository, String> {
+    let repo = Repository::open(path);
+
+    let result = match repo {
+        Ok(repo) => {
+            if validate_repo(&repo) {
+                Ok(repo)
+            } else {
+                Err("Unable to open repo".to_string())
+            }
+        }
+        Err(_) => Err("Unable to open repo".to_string()),
+    };
+
+    return result;
 }
 
 fn main() {
-    let args = Forge::parse();
+    let args = CliArgs::parse();
 
     println!("Reading repo at {:?} ... ", args.path);
-    check_repo(args.path);
+
+    let repo = get_repo(args.path); // Its okay if we panic here
+
+    match repo {
+        Ok(..) => println!("Valid repo!"),
+        Err(..) => panic!("Failed to get and validate repository"),
+    }
 }
